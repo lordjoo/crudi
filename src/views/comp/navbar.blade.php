a3eeb60b0ac5e543fe494b329f830ca16554b545
lordjoo <youssef.afify122@gmail.com>
2020-10-30 21:20:15 +0200

    add loguot button

--- src/views/comp/navbar.blade.php
+++ src/views/comp/navbar.blade.php
@@ -3,15 +3,36 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-light white scrolling-navbar">
     <div class="container-fluid">
 
-        <!-- Brand -->
-        <a class="navbar-brand black-text">
-            <strong class="">Dashboard</strong>
+        <a class="navbar-brand" href="#">
+            Dashboard
         </a>
 
-        <!-- Collapse -->
-        <button class="navbar-toggler sideNavToggle" type="button">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+
+        <!-- Links -->
+        <div class="d-flex align-items-center" id="basicExampleNav">
+            <!-- Right -->
+            <ul class="navbar-nav nav-flex-icons  align-items-center">
+                <li class="nav-item avatar dropdown is-auth">
+                    <a class="nav-link dropdown-toggle d-flex align-items-center " id="navbarDropdownMenuLink-55" data-toggle="dropdown"
+                       aria-haspopup="true" aria-expanded="false">
+                        <span class="mr-2 account-name d-md-block d-none" id="account-name">{{ auth()->user()->name }}</span>
+                        <span class="fas fa-user-circle"></span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-md-right dropdown-menu-right dropdown-dark"
+                         aria-labelledby="navbarDropdownMenuLink-55">
+                        <a onclick="document.getElementById('logout_form').submit()" class="dropdown-item">Log Out</a>
+                        <form id="logout_form" method="POST" action="{{ route('logout') }}">@csrf</form>
+                    </div>
+                </li>
+            </ul>
+            <!-- Collapse button -->
+            <button class="navbar-toggler sideNavToggle" type="button">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+        </div>
+
+
     </div>
 </nav>
 <!-- Navbar -->
