ea4416193da1d4ad1bbdb41a01ee3042889c4514
lordjoo <youssef.afify122@gmail.com>
2020-11-05 15:53:30 +0200

    Fix some bugs in select input

--- src/views/fields/select.blade.php
+++ src/views/fields/select.blade.php
@@ -1,5 +1,5 @@
 <select @if(isset($field['required']) && $field['required']) required @endif name="{{ $c }}" class="mdb-select md-form">
-    <option value="" disabled selected>{{ $name }}</option>
+    <option value="" disabled  @if(!isset($val)) selected @endif>{{ $name }}</option>
     @foreach($field['options'] as $q)
     <option @if(isset($val) && $val == $q) selected @endif value="{{ $q }}">{{ $q }}</option>
     @endforeach
