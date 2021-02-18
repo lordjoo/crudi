5f1f5bf6930a26bd71cde2478d872626b13d0577
lordjoo <lordjoo@users.noreply.github.com>
2021-02-18 18:33:18 +0200

    update tinymce config values

--- src/views/fields/editor.blade.php
+++ src/views/fields/editor.blade.php
@@ -9,12 +9,13 @@
             path_absolute: "/admin/",
             selector: '#{{$c}}',
             height: 500,
-            contextmenu:"link table configurepermanentpen",
+            contextmenu:"link table",
             image_advtab: true,
             relative_urls: false,
-            quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote',
             plugins: 'preview fullpage searchreplace autolink visualblocks visualchars image link media table charmap hr pagebreak nonbreaking anchor lists textcolor wordcount tinymcespellchecker imagetools mediaembed quickbars linkchecker contextmenu colorpicker',
             toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link image | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
+            quickbars_insert_toolbar: '',
+            quickbars_selection_toolbar: 'bold italic underline | formatselect | bullist numlist | blockquote quicklink',
             image_advtab: true,
             file_picker_callback: function (callback, value, meta) {
                 var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
