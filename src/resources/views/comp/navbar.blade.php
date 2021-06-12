d3ce4633077ed302b002da1b92ea0e8d466e5c60
Youssef Mahmoud <youssef.afify122@gmail.com>
2021-06-12 10:34:05 +0200

    Update navbar.blade.php

--- src/resources/views/comp/navbar.blade.php
+++ src/resources/views/comp/navbar.blade.php
@@ -14,7 +14,7 @@ if (file_exists($menu_file)) {
         </svg>
     </button>
     <a class="c-header-brand d-lg-none c-header-brand-sm-up-center" href="{{ config('crudi.home_url','/admin') }}">
-       <img src='{{ config('crudi.logo_path') }}' />
+       <img height="25" src='{{ config('crudi.logo_path_2') }}' />
     </a>
 
 
